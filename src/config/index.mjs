4f3cd3671a86bd8857347c16ea653f174c577bb3
Slavik <theslavprod@gmail.com>
2023-07-02 19:16:53 +0400

    delete trim

--- src/config/index.mjs
+++ src/config/index.mjs
@@ -2,12 +2,12 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const DB_CONFIG = {
-    DATABASE: process.env.DB_DATABASE.trim(),
-    USERNAME: process.env.DB_USERNAME.trim(),
-    PASSWORD: process.env.DB_PASSWORD.trim(),
-    HOST: process.env.DB_HOST.trim(),
-    PORT: process.env.DB_PORT.trim(),
+    DATABASE: process.env.DB_DATABASE,
+    USERNAME: process.env.DB_USERNAME,
+    PASSWORD: process.env.DB_PASSWORD,
+    HOST: process.env.DB_HOST,
+    PORT: process.env.DB_PORT,
 }
 
-export const PORT = +process.env.PORT.trim();
-export const JWT_SECRET = process.env.JWT_SECRET.trim();
+export const PORT = process.env.PORT;
+export const JWT_SECRET = process.env.JWT_SECRET;
